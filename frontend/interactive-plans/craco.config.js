5361f8b32589108d57f242dcbdc26ec909e9e314
Artur Yakupov <art.kazan@mail.ru>
2025-04-02 11:32:42 +0300

    added uncommited files due to gitignore issue

---
+++ frontend/interactive-plans/craco.config.js
@@ -0,0 +1,16 @@
+const CracoAlias = require("craco-alias");
+
+module.exports = {
+    plugins: [
+        {
+            plugin: CracoAlias,
+            options: {
+                source: "tsconfig",
+                // baseUrl SHOULD be specified
+                // plugin does not take it from tsconfig
+                baseUrl: "./src",
+                tsConfigPath: "./tsconfig.extend.json"
+            }
+        }
+    ]
+};
